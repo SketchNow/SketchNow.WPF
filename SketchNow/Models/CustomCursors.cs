0cb52f68787e79528ba23f526b503320cc0a4e79
realybin <realybin@outlook.com>
2024-11-27 17:11:41 +0800

    fix: ci error on `cursor` chore: document style

--- SketchNow/Models/CustomCursors.cs
+++ SketchNow/Models/CustomCursors.cs
@@ -3,10 +3,26 @@ using System.Windows.Input;
 
 namespace SketchNow.Models;
 
-public class CustomCursors
+public interface ICustomCursors
 {
-    public Cursor Remove { get; } = new(Application.GetResourceStream(new Uri(@"Resources\Remove.cur", UriKind.Relative))?.Stream ?? throw new CursorNotFoundException(@"Resources\Remove.cur"));
-    public Cursor Circle { get; } = new(Application.GetResourceStream(new Uri(@"Resources\Circle.cur", UriKind.Relative))?.Stream ?? throw new InvalidOperationException(@"Resources\Circle.cur"));
-    public Cursor Select { get; } = new(Application.GetResourceStream(new Uri(@"Resources\Select.cur", UriKind.Relative))?.Stream ?? throw new InvalidOperationException(@"Resources\Select.cur"));
+    Cursor Remove { get; }
+    Cursor Circle { get; }
+    Cursor Select { get; }
 }
-public class CursorNotFoundException(string message) : Exception(message);
+
+public class CustomCursors : ICustomCursors
+{
+    private static Cursor LoadCursor(string resourcePath)
+    {
+        var uri = new Uri($"/SketchNow;component/Resources/{resourcePath}", UriKind.Relative);
+        var stream = Application.GetResourceStream(uri)?.Stream 
+                     ?? throw new CursorNotFoundException(resourcePath);
+        return new Cursor(stream);
+    }
+
+    public Cursor Remove { get; } = LoadCursor("Remove.cur");
+    public Cursor Circle { get; } = LoadCursor("Circle.cur");
+    public Cursor Select { get; } = LoadCursor("Select.cur");
+}
+
+public class CursorNotFoundException(string message) : Exception($"Cursor not found: {message}");
